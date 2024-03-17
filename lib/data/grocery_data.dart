314cf3dd9dc93cf27d7f6a5fb7bfd821032dea3a
Pascal JK <pascual.santos80@gmail.com>
2024-03-17 14:17:38 +0200

    - added groceries data file

---
+++ lib/data/grocery_data.dart
@@ -0,0 +1,74 @@
+class GroceryData {
+  static List<Map<String, dynamic>> groceryProducts = [
+    {
+      "id": 1,
+      "name": "Apples",
+      "description": "Fresh, juicy apples",
+      "price": 2.99,
+      "image_url": "https://th.bing.com/th/id/OIP.0gSdYEt2_i9ZZGD00SradAHaHa?rs=1&pid=ImgDetMain",
+    },
+    {
+      "id": 2,
+      "name": "Bananas",
+      "description": "Ripe bananas",
+      "price": 1.99,
+      "image_url": "https://th.bing.com/th/id/R.798f505be8e67d2d90b7e82360913944?rik=eib%2fePoiEExoRg&pid=ImgRaw&r=0"
+    },
+    {
+      "id": 3,
+      "name": "Milk",
+      "description": "Fresh dairy milk",
+      "price": 3.49,
+      "image_url": "https://th.bing.com/th/id/R.018189ae1187136c8e3d30ed9f0453ac?rik=YFN3CW%2bPujil6g&pid=ImgRaw&r=0",
+    },
+    {
+      "id": 4,
+      "name": "Bread",
+      "description": "Whole wheat bread",
+      "price": 2.29,
+      "image_url": "https://www.tasteofhome.com/wp-content/uploads/2018/01/exps372_MBM1450751D25B_WEB.jpg"
+    },
+    {
+      "id": 5,
+      "name": "Eggs",
+      "description": "Farm-fresh eggs",
+      "price": 4.99,
+      "image_url": "https://th.bing.com/th/id/R.f92d6b8c632fff90c156d494b48d6dbf?rik=d%2fYk3yZemjm0KQ&pid=ImgRaw&r=0",
+    },
+    {
+      "id": 6,
+      "name": "Cheese",
+      "description": "Cheddar cheese block",
+      "price": 5.79,
+      "image_url": "https://th.bing.com/th/id/R.2192dbf9c83f5909adf5eacc4836c4f0?rik=QBS0MU%2bLivWdlA&pid=ImgRaw&r=0",
+    },
+    {
+      "id": 7,
+      "name": "Chicken",
+      "description": "Boneless, skinless chicken breast",
+      "price": 6.99,
+      "image_url": "https://th.bing.com/th/id/R.da66b33de374358017252ed08a646212?rik=uNJTW7%2bnO090yg&pid=ImgRaw&r=0",
+    },
+    {
+      "id": 8,
+      "name": "Potatoes",
+      "description": "Russet potatoes",
+      "price": 3.99,
+      "image_url": "https://th.bing.com/th/id/OIP.nhnysvh-yu6cPRhcuOiwwAHaEK?rs=1&pid=ImgDetMain",
+    },
+    {
+      "id": 9,
+      "name": "Tomatoes",
+      "description": "Vine-ripened tomatoes",
+      "price": 2.49,
+      "image_url": "https://th.bing.com/th/id/OIP.kvhd4pDdoEqaKpsCnkTCdwHaE7?rs=1&pid=ImgDetMain",
+    },
+    {
+      "id": 10,
+      "name": "Spinach",
+      "description": "Fresh spinach leaves",
+      "price": 1.79,
+      "image_url": "https://th.bing.com/th/id/OIP.UjYirlKTHtygYKg7lzHOVwHaE8?rs=1&pid=ImgDetMain",
+    },
+  ];
+}
