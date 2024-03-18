6d267f2845113fd6c5d9716752b5d6d26d027940
Pascal JK <pascual.santos80@gmail.com>
2024-03-18 12:54:04 +0200

    - added new widget.

---
+++ lib/pages/home/widgets/product_item.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import '../../../models/product.dart';
+
+class ProductItem extends StatelessWidget {
+  final ProductModel model;
+  const ProductItem({super.key, required this.model});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
