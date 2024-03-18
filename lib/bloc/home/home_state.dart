7d84df0c60b71f243f87e18130cc13c2b462fe0e
Pascal JK <pascual.santos80@gmail.com>
2024-03-18 11:56:54 +0200

    - working on home state file.

--- lib/bloc/home/home_state.dart
+++ lib/bloc/home/home_state.dart
@@ -3,4 +3,16 @@ part of 'home_bloc.dart';
 @immutable
 sealed class HomeState {}
 
+sealed class HomeActionState extends HomeState {}
+
 final class HomeInitial extends HomeState {}
+
+final class HomeLoadingState extends HomeState {}
+
+final class HomeLoadedSuccessState extends HomeState {}
+
+final class HomeErrorState extends HomeState {}
+
+final class HomeNavigateToWishlistPageActionState extends HomeActionState {}
+
+final class HomeNavigateToCartPageActionState extends HomeActionState {}
