9ab694300b9717522a18f462743da731e8fb2bff
Pascal JK <pascual.santos80@gmail.com>
2024-03-18 20:18:30 +0200

    - working on cart page file.

--- lib/pages/cart/cart_page.dart
+++ lib/pages/cart/cart_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../bloc/cart/cart_bloc.dart';
+import 'widgets/cart_item.dart';
 
 class CartPage extends StatefulWidget {
   const CartPage({super.key});
@@ -8,11 +12,53 @@ class CartPage extends StatefulWidget {
 }
 
 class _CartPageState extends State<CartPage> {
+  final cartBloc = CartBloc();
+
+  @override
+  void initState() {
+    cartBloc.add(CartInitialEvent());
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Cart Page'),
+      ),
+      body: BlocConsumer<CartBloc, CartState>(
+        bloc: cartBloc,
+        listener: (context, state) {
+          if (state is CartItemRemoved) {
+            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Item removed')));
+          }
+        },
+        listenWhen: (previous, current) => current is CartActoinState,
+        buildWhen: (previous, current) => current is! CartActoinState,
+        builder: (context, state) {
+          switch (state.runtimeType) {
+            case CartSuccuessState:
+              final successState = state as CartSuccuessState;
+              // return AnimatedList(
+              //   padding: const EdgeInsets.all(10),
+              //   itemBuilder: (context, index, animation) => CartItem(
+              //     model: successState.cartItems[index],
+              //     cartBloc: cartBloc,
+              //   ),
+              //   initialItemCount: successState.cartItems.length,
+              // );
+              return ListView.builder(
+                padding: const EdgeInsets.all(10),
+                itemBuilder: (context, index) => CartItem(
+                  model: successState.cartItems[index],
+                  cartBloc: cartBloc,
+                ),
+                itemCount: successState.cartItems.length,
+              );
+            default:
+              return const SizedBox();
+          }
+        },
       ),
     );
   }
