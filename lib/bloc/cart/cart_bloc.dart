1e2fdea002b94279b436f05af0996b6471d20def
Pascal JK <pascual.santos80@gmail.com>
2024-03-18 19:05:50 +0200

    - working on cart bloc file.

--- lib/bloc/cart/cart_bloc.dart
+++ lib/bloc/cart/cart_bloc.dart
@@ -1,5 +1,10 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
+import 'package:bloc_app/models/product.dart';
 import 'package:meta/meta.dart';
+
+import '../../data/cart_items.dart';
 
 part 'cart_event.dart';
 part 'cart_state.dart';
@@ -7,7 +12,11 @@ part 'cart_state.dart';
 class CartBloc extends Bloc<CartEvent, CartState> {
   CartBloc() : super(CartInitial()) {
     on<CartEvent>((event, emit) {
-      // TODO: implement event handler
+      on<CartInitialEvent>(cartInitialEvent);
     });
+  }
+
+  FutureOr<void> cartInitialEvent(CartInitialEvent event, Emitter<CartState> emit) {
+    emit(CartSuccuessState(cartItems: cartItems));
   }
 }
