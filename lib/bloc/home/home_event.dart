89eab76dc53f1acc52a7a67a3698370bcadb2303
Pascal JK <pascual.santos80@gmail.com>
2024-03-18 11:57:18 +0200

    - working on home event file.

--- lib/bloc/home/home_event.dart
+++ lib/bloc/home/home_event.dart
@@ -2,3 +2,11 @@ part of 'home_bloc.dart';
 
 @immutable
 sealed class HomeEvent {}
+
+class HomeProductCartButtonClickedEvent extends HomeEvent {}
+
+class HomeProductWishlistButtonClickedEvent extends HomeEvent {}
+
+class HomeCartButtonNavigateEvent extends HomeEvent {}
+
+class HomeWishlistButtonNavigateEvent extends HomeEvent {}
