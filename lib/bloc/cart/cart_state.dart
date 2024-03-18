bbd98f166b9bd291e85022847d874c055be30bc2
Pascal JK <pascual.santos80@gmail.com>
2024-03-18 19:05:13 +0200

    - working on cart state file.

--- lib/bloc/cart/cart_state.dart
+++ lib/bloc/cart/cart_state.dart
@@ -2,9 +2,17 @@ part of 'cart_bloc.dart';
 
 @immutable
 sealed class CartState {}
+
 sealed class CartActoinState extends CartState {}
 
 final class CartInitial extends CartState {}
-final class CartSuccuessState extends CartState {}
+
+final class CartSuccuessState extends CartState {
+  final List<ProductModel> cartItems;
+
+  CartSuccuessState({required this.cartItems});
+}
+
 final class CartErrorState extends CartState {}
+
 final class CartLoadingState extends CartState {}
