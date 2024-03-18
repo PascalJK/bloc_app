e4e33e95b9f2b0ce3d426237d4b6f9b2cc2a36e4
Pascal JK <pascual.santos80@gmail.com>
2024-03-18 18:35:52 +0200

    - changed cart page from stateless to stateful widget.

--- lib/pages/cart/cart_page.dart
+++ lib/pages/cart/cart_page.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class CartPage extends StatelessWidget {
+class CartPage extends StatefulWidget {
   const CartPage({super.key});
 
+  @override
+  State<CartPage> createState() => _CartPageState();
+}
+
+class _CartPageState extends State<CartPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
