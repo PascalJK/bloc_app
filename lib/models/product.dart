47c8af41acb3d0812a778bf56bbf7a7a6c5a4e36
Pascal JK <pascual.santos80@gmail.com>
2024-03-18 20:32:34 +0200

    - working on product model file (getters)

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,3 +1,6 @@
+import 'package:bloc_app/data/cart_items.dart';
+import 'package:bloc_app/data/wishlist_items.dart';
+
 class ProductModel {
   final String id;
   final String name;
@@ -14,6 +17,16 @@ class ProductModel {
   });
 
   String get getPriceString => 'N\$ $price';
+  
+  bool get isCarted {
+    if (cartItems.isEmpty) return false;
+    return cartItems.firstOrNull?.id == id;
+  }
+
+  bool get isWishlisted {
+    if (wishlistItems.isEmpty) return false;
+    return wishlistItems.firstOrNull?.id == id;
+  }
 
   factory ProductModel.fromMap(map) {
     return ProductModel(
