e9d6ad6b7aa3f923925e0ac7c915556e218b8df0
Pascal JK <pascual.santos80@gmail.com>
2024-03-18 12:24:28 +0200

    - working on home page listeners.

--- lib/pages/home/homepage.dart
+++ lib/pages/home/homepage.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../bloc/home/home_bloc.dart';
+import '../cart/cart_page.dart';
+import '../wishlist/wishlist_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -16,9 +18,16 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return BlocConsumer<HomeBloc, HomeState>(
       bloc: homeBloc,
-      buildWhen: (previous, current) => current is !HomeActionState,
-      listener: (context, state) {},
+      buildWhen: (previous, current) => current is! HomeActionState,
       listenWhen: (previous, current) => current is HomeActionState,
+      listener: (context, state) {
+        if (state is HomeNavigateToCartPageActionState) {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => CartPage()));
+        }
+        else if (state is HomeNavigateToWishlistPageActionState) {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => WishlistPage()));
+        }
+      },
       builder: (context, state) {
         return Scaffold(
           appBar: AppBar(
